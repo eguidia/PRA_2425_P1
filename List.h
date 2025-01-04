cca1bf01ee0df0793e9bc6cbeda9deef252b3020
eguidia <ernestguillend@gmail.com>
2025-01-04 18:33:03 +0100

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -1,28 +1,20 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdexcept>
-
-template <typename T>
+template <typename T> 
 class List {
 public:
-    virtual ~List() {}
+    virtual void insert(int pos, T e) = 0; // Método insert
+    virtual void append(T e) = 0;          // Método append
+    virtual void prepend(T e) = 0;         // Método prepend
+    virtual T remove(int pos) = 0;         // Método remove
+    virtual T get(int pos) = 0;            // Método get
+    virtual int search(T e) = 0;           // Método search
+    virtual bool empty() = 0;              // Método empty
+    virtual int size() = 0;                // Método size
 
-    virtual void insert(int pos, T e) = 0;
-
-    virtual void append(T e) = 0;
-
-    virtual void prepend(T e) = 0;
-
-    virtual T remove(int pos) = 0;
-
-    virtual T get(int pos) const = 0;
-
-    virtual int search(T e) const = 0;
-
-    virtual bool empty() const = 0;
-
-    virtual int size() const = 0;
+    virtual ~List() {} // Destructor virtual
 };
 
 #endif
+
