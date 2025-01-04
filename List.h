148300d476b3dbd5c425df7d770dc41c3ed7969c
eguidia <ernestguillend@gmail.com>
2025-01-04 19:32:21 +0100

    Implementación de la lista enlazada y código de prueba

--- List.h
+++ List.h
@@ -1,19 +1,16 @@
 #ifndef LIST_H
 #define LIST_H
 
-template <typename T> 
+template <typename T>
 class List {
 public:
-    virtual void insert(int pos, T e) = 0; // Método insert
-    virtual void append(T e) = 0;          // Método append
-    virtual void prepend(T e) = 0;         // Método prepend
-    virtual T remove(int pos) = 0;         // Método remove
-    virtual T get(int pos) = 0;            // Método get
-    virtual int search(T e) = 0;           // Método search
-    virtual bool empty() = 0;              // Método empty
-    virtual int size() = 0;                // Método size
-
-    virtual ~List() {} // Destructor virtual
+    virtual void insert(int pos, const T& data) = 0;
+    virtual T remove(int pos) = 0;
+    virtual T get(int pos) const = 0;
+    virtual int size() const = 0;
+    virtual bool empty() const = 0;
+    virtual int search(const T& data) const = 0;
+    virtual ~List() {}
 };
 
 #endif
--- Node.h
+++ Node.h
@@ -1,8 +1,5 @@
-// Node.h
 #ifndef NODE_H
 #define NODE_H
-
-#include <iostream>
 
 template <typename T>
 class Node {
@@ -10,10 +7,8 @@ public:
     T data;
     Node<T>* next;
 
-    // Constructor
     Node(T data, Node<T>* next = nullptr) : data(data), next(next) {}
 
-    // Sobrecarga del operador <<
     friend std::ostream& operator<<(std::ostream &out, const Node<T> &node) {
         out << node.data;
         return out;
